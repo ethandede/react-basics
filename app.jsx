0cbc8010ac724b144d3c0cabcd7997121661285d
ethandede <ethanede@gmail.com>
2017-04-12 12:19:40 -0700

    Added additional elements

--- app.jsx
+++ app.jsx
@@ -1,9 +1,25 @@
 function Application() {
     return (
-        <div>
-            <h1>Hello from React</h1>
-            <p>I was renedered from the Application component!</p>
+        <div className="scoreboard">
+            <div className="header">
+                <h1>Scoreboard</h1>
+            </div>
+
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                    Ethan Ede
+                </div>
+                <div className="player-score">
+                    <div className="counter">
+                        <button className="counter-action decrement"> - </button>
+                        <div className="counter-score"> 31 </div>
+                        <button className="counter-action increment"> + </button>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     );
 }
 
