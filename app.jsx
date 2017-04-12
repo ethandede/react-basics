23b8ddd603d9106e69546b8c2e20372e022f884f
ethandede <ethanede@gmail.com>
2017-04-12 13:14:41 -0700

    Added additional elements

--- app.jsx
+++ app.jsx
@@ -1,26 +1,55 @@
-function Application() {
+function Header(props) {
     return (
-        <div className="scoreboard">
-            <div className="header">
-                <h1>Scoreboard</h1>
-            </div>
+        <div className="header">
+            <h1>{props.title}</h1>
+        </div>
+    );
+}
 
-            <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                    Ethan Ede
-                </div>
-                <div className="player-score">
-                    <div className="counter">
-                        <button className="counter-action decrement"> - </button>
-                        <div className="counter-score"> 31 </div>
-                        <button className="counter-action increment"> + </button>
-                    </div>
+Header.propTypes = {
+    title:  React.PropTypes.string.isRequired,
+};
+
+Player.propTypes = {
+    name:  React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+};
+
+function Player(props) {
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <div className="player-score">
+                <div className="counter">
+                    <button className="counter-action decrement"> - </button>
+                    <div className="counter-score"> {props.score} </div>
+                    <button className="counter-action increment"> + </button>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
+
+function Application(props) {
+    return (
+        <div className="scoreboard">
+            <Header title={props.title} />
+            <div className="players">
+                <Player name="Jim Hoskins" score={31} />
+                <Player name="Ethan Ede" score={33} />
+            </div>
+        </div>
+    );
+}
+
+Application.propTypes = {
+    title:  React.PropTypes.string.isRequired,
+};
+
+Application.defaultProps = {
+    title: "Scoreboard"
+};
 
 ReactDOM.render(<Application />, document.getElementById("container"));
